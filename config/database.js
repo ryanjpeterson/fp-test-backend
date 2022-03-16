692fff66283e2370e591a8809bf2b442ba6577bb
ryan_peterson <ryanjay333@gmail.com>
2022-03-16 15:25:37 -0400

    digital ocean config

--- config/database.js
+++ config/database.js
@@ -1,11 +1,29 @@
-const path = require('path');
+if (env('NODE_ENV') === 'production') {
+  return {
+    connection: {
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+        },
+      },
+      debug: false,
+    },
+  };
+}
 
-module.exports = ({ env }) => ({
+return {
   connection: {
     client: 'sqlite',
     connection: {
       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
     useNullAsDefault: true,
-  },
-});
+  }
+}  
+};
--- config/env/production/server.js
+++
@@ -1,4 +0,0 @@
-module.exports = ({ env }) => ({
-    url: env('https://obscure-taiga-92704.herokuapp.com/'),
-  });
-   
